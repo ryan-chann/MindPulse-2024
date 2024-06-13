93efa69672a5da9d016ce94a2bb1c3321aee00b2
Ryan Chan <ryanchanjy@outlook.com>
2024-06-14 03:17:54 +0800

    Initialized DynamoDbConfig

---
+++ MindPulse_API/mindpulse/src/main/java/edu/mingjun/mindpulse/MindpulseApplication.java
@@ -0,0 +1,15 @@
+package edu.mingjun.mindpulse;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class MindpulseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindpulseApplication.class, args);
+	}
+
+}
